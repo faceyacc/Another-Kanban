384f5d2c6066b14bf3d085ab791fd770626606df
Ty <justfacey@gmail.com>
2021-09-03 14:13:05 -0400

    used can save tasks and list.

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -1,65 +1,58 @@
-import { createContext, useContext, Dispatch , FC ,useEffect } from "react"
+import { createContext, useContext, useEffect, Dispatch } from "react"
 import { useImmerReducer } from "use-immer"
-import { Action } from './actions'
-import { appStateReducer, AppState, List, Task } from "./appStateReducer"
+import { Action } from "./actions"
+import {
+  appStateReducer,
+  AppState,
+  List,
+  Task
+} from "./appStateReducer"
 import { DragItem } from "../DragItem"
-import { save } from "../api"
-
-
-
+import { withInitialState } from "../withInitialState"
+import { save } from "../api"
 
 type AppStateContextProps = {
-    draggedItem: DragItem | null
-    lists: List[]
-    getTasksByListId(id: string): Task[]
-    dispatch: Dispatch<Action>
+  draggedItem: DragItem | null
+  lists: List[]
+  getTasksByListId(id: string): Task[]
+  dispatch: Dispatch<Action>
 }
 
-// Creates context for AppState
-const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps)
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+)
 
-// Obtains context of AppStateContext 
-export const useAppState = () => {
-    return useContext(AppStateContext)
+type AppStateProviderProps = {
+  children: React.ReactNode
+  initialState: AppState
 }
 
-const appData: AppState = {
-    draggedItem: null,
-    lists: [
-        {
-            id: "0", 
-            text: "To Do", 
-            tasks: [{ id: "c0", text: "Generate app scaffold" }]
-        },
-        {
-            id: "1", 
-            text: "In Progress", 
-            tasks: [{ id: "c2", text: "Learn Typescript & Python" }]
-        },
-        {
-            id: "2", 
-            text: "Done", 
-            tasks: [{ id: "c3", text: "Begin to use static typing" }]
-        }
-    ]
-}
+export const AppStateProvider = withInitialState<AppStateProviderProps>(
+  ({ children, initialState }) => {
+    const [state, dispatch] = useImmerReducer(
+      appStateReducer,
+      initialState
+    )
 
-
-export const AppStateProvider: FC = ({children}) => {
-    const [state, dispatch] = useImmerReducer(appStateReducer, appData)
+    useEffect(() => {
+      save(state)
+    }, [state])
 
     const { draggedItem, lists } = state
     const getTasksByListId = (id: string) => {
-        return lists.find((list) => list.id===id)?.tasks || []
+      return lists.find((list) => list.id === id)?.tasks || []
     }
 
-    useEffect(() => {
-        save(state)
-    }, [state])
-
     return (
-        <AppStateContext.Provider value={{ draggedItem , lists, getTasksByListId, dispatch }}>
-            {children}
-        </AppStateContext.Provider>
+      <AppStateContext.Provider
+        value={{ draggedItem, lists, getTasksByListId, dispatch }}
+      >
+        {children}
+      </AppStateContext.Provider>
     )
+  }
+)
+
+export const useAppState = () => {
+  return useContext(AppStateContext)
 }
--- src/withInitialState.tsx
+++ src/withInitialState.tsx
@@ -1,29 +1,50 @@
-import { useState, useState, ComponentType } from "react"
+import { useState, useEffect } from "react"
 import { AppState } from "./state/appStateReducer"
-
+import { load } from "./api"
 
 type InjectedProps = {
-    initialState: AppState
+  initialState: AppState
 }
 
-export function withInitalState<TProps>(WrappedComponent: ComponentType<TProps & InjectedProps>)   {
-    return (props: Omit<TProps, keyof InjectedProps>) => {
-        const [initialState, setInitalState] = useState<AppState>({
-            lists: [],
-            draggedItem: null
-        })
+type PropsWithoutInjected<TBaseProps> = Omit<
+  TBaseProps,
+  keyof InjectedProps
+>
 
+export function withInitialState<TProps>(
+  WrappedComponent: React.ComponentType<
+    PropsWithoutInjected<TProps> & InjectedProps
+  >
+) {
+  return (props: PropsWithoutInjected<TProps>) => {
+    const [initialState, setInitialState] = useState<AppState>({
+      lists: [],
+      draggedItem: null
+    })
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState<Error | undefined>()
 
-        return (
-            <WrappedComponent
-                {...props as TProps}
-                initialState={initialState}
-            />
-        )
+    useEffect(() => {
+      const fetchInitialState = async () => {
+        try {
+          const data = await load()
+          setInitialState(data)
+        } catch (e: any) {
+          setError(e)
+        }
+        setIsLoading(false)
+      }
+      fetchInitialState()
+    }, [])
 
-
-
-
+    if (isLoading) {
+      return <div>Loading</div>
     }
 
+    if (error) {
+      return <div>{error.message}</div>
+    }
+
+    return <WrappedComponent {...props} initialState={initialState} />
+  }
 }
